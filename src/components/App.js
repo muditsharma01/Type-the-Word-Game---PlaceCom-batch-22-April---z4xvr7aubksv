b6ffc51f75108eb25f35a5e73f2f0b99e352d57b
Mudit Sharma <muditshar001@gmail.com>
2023-04-22 19:13:16 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -7,55 +7,57 @@ function App() {
   const [word, setWord] = useState('');
   const [flashWord, setFlashWord] = useState(true);
   const [userInput, setUserInput] = useState('');
-  const [result, setResult] = useState('');
+  const [result, setResult] = useState(null);
   const [index, setIndex] = useState(0);
 
   useEffect(() => {
-    // Display the first word from WORD_LIST for 500ms on page load
-    setTimeout(() => {
-      setWord(WORD_LIST[index]);
+    // Set the first word from the WORD_LIST array as the initial word
+    setWord(WORD_LIST[index]);
+
+    // After 500ms, hide the word and show the input form
+    const timerId = setTimeout(() => {
       setFlashWord(false);
     }, 500);
-  }, []);
 
-  const handleInputChange = (event) => {
+    // Clear the timer on component unmount
+    return () => clearTimeout(timerId);
+  }, [index]);
+
+  function handleInputChange(event) {
     setUserInput(event.target.value);
   }
 
-  const handleFormSubmit = (event) => {
+  function handleFormSubmit(event) {
     event.preventDefault();
-    if (userInput === word) {
+
+    // Check if the user input matches the current word from WORD_LIST
+    if (userInput.toLowerCase() === WORD_LIST[index].toLowerCase()) {
       setResult('You Won!');
     } else {
       setResult('You Lost!');
     }
   }
 
-  const handleRestartClick = () => {
-    setResult('');
+  function handleRestartClick() {
+    // Reset the state variables to their initial values
+    setIndex(0);
     setUserInput('');
+    setResult(null);
     setFlashWord(true);
-    setTimeout(() => {
-      setIndex(index + 1);
-      setWord(WORD_LIST[index + 1]);
-      setFlashWord(false);
-    }, 500);
   }
 
   return (
     <div className="mini-game-container">
       <h2 className="mini-game-title">Mini Game</h2>
       {flashWord && (
-        <p className="mini-game-word">{WORD_LIST[index]}</p>
+        <p className="mini-game-word">{word}</p>
       )}
-      <form className="mini-game-form" onSubmit={handleFormSubmit}>
-        {!flashWord && (
+      {!flashWord && (
+        <form className="mini-game-form" onSubmit={handleFormSubmit}>
           <input className="mini-game-input" type="text" value={userInput} onChange={handleInputChange} />
-        )}
-        {!flashWord && (
           <button className="mini-game-button" type="submit">Check Answer</button>
-        )}
-      </form>
+        </form>
+      )}
       {result && (
         <>
           <p className="mini-game-result">{result}</p>
