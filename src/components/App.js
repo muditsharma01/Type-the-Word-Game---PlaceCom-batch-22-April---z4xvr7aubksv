ca5df393b5ee7a1209867e256cdcb09baae9f178
Mudit Sharma <muditshar001@gmail.com>
2023-04-22 19:09:09 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -10,19 +10,56 @@ function App() {
   const [result, setResult] = useState('');
   const [index, setIndex] = useState(0);
 
+  useEffect(() => {
+    // Display the first word from WORD_LIST for 500ms on page load
+    setTimeout(() => {
+      setWord(WORD_LIST[index]);
+      setFlashWord(false);
+    }, 500);
+  }, []);
+
+  const handleInputChange = (event) => {
+    setUserInput(event.target.value);
+  }
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    if (userInput === word) {
+      setResult('You Won!');
+    } else {
+      setResult('You Lost!');
+    }
+  }
+
+  const handleRestartClick = () => {
+    setResult('');
+    setUserInput('');
+    setFlashWord(true);
+    setTimeout(() => {
+      setIndex(index + 1);
+      setWord(WORD_LIST[index + 1]);
+      setFlashWord(false);
+    }, 500);
+  }
 
   return (
-    <div class="mini-game-container">
-      <h2 class="mini-game-title">Mini Game</h2>
-      <p class="mini-game-word">{word}</p>
-      <form class="mini-game-form" onSubmit={handleFormSubmit}>
-        <input class="mini-game-input" type="text" value={userInput} onChange={handleInputChange} />
-        <button class="mini-game-button" type="submit">Check Answer</button>
+    <div className="mini-game-container">
+      <h2 className="mini-game-title">Mini Game</h2>
+      {flashWord && (
+        <p className="mini-game-word">{WORD_LIST[index]}</p>
+      )}
+      <form className="mini-game-form" onSubmit={handleFormSubmit}>
+        {!flashWord && (
+          <input className="mini-game-input" type="text" value={userInput} onChange={handleInputChange} />
+        )}
+        {!flashWord && (
+          <button className="mini-game-button" type="submit">Check Answer</button>
+        )}
       </form>
       {result && (
         <>
-          <p class="mini-game-result">{result}</p>
-          <button class="mini-game-restart-button" onClick={handleRestartClick}>Restart</button>
+          <p className="mini-game-result">{result}</p>
+          <button className="mini-game-restart-button" onClick={handleRestartClick}>Restart</button>
         </>
       )}
     </div>
